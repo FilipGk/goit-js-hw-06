63e6654a3c206fc4db94fda2552510a43e981fe8
FilipGk <skomurlight@gmail.com>
2023-02-18 21:18:08 +0100

    8,9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector(".change-color");
+btn.addEventListener("click", function () {
+  let newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  document.querySelector(".color").innerHTML = newColor;
+});
