3b26cebe6368c36f1a1cf98b629280ecd810eaf3
FilipGk <skomurlight@gmail.com>
2023-02-19 20:02:59 +0100

    Corrected

--- js/task-01.js
+++ js/task-01.js
@@ -1,34 +1,11 @@
-const categories = document.querySelector("#categories");
-const numberOfCategories = categories.children.length;
-const firstCategory = document.querySelector(".item").firstElementChild.textContent;
-const firstCategoryElements = document.querySelector(".item").querySelector("ul").children.length;
-const secondCategory = document
-  .querySelector(".item")
-  .nextElementSibling.querySelector("h2").textContent;
-const secondCategoryElements = document
-  .querySelector(".item")
-  .nextElementSibling.querySelector("ul").children.length;
-const thirdCategory = document
-  .querySelector(".item")
-  .nextElementSibling.nextElementSibling.querySelector("h2").textContent;
-const thirdCategoryElements = document
-  .querySelector(".item")
-  .nextElementSibling.nextElementSibling.querySelector("ul").children.length;
-console.log(
-  `Number of categories: ${numberOfCategories}`,
-  "\n",
-  "\n",
-  `Category: ${firstCategory}`,
-  "\n",
-  `Elements: ${firstCategoryElements}`,
-  "\n",
-  "\n",
-  `Category: ${secondCategory}`,
-  "\n",
-  `Elements: ${secondCategoryElements}`,
-  "\n",
-  "\n",
-  `Category: ${thirdCategory}`,
-  "\n",
-  `Elements: ${thirdCategoryElements}`
-);
+const categoriesList = document.querySelector("#categories");
+
+const categoriesItems = categoriesList.querySelectorAll("li.item");
+
+console.log(`Number of categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach((item) => {
+  const categoryName = item.querySelector("h2").textContent;
+  const categoryElements = item.querySelectorAll("li").length;
+  console.log(`Category: ${categoryName}\nElements: ${categoryElements}\n`);
+});
